889064949d20ecd60e644ebedc3f173254b750b7
chetan <chetan@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-03-17 19:52:22 +0000

    Styling is now automatic. Description updated.
    git-svn-id: https://plugins.svn.wordpress.org/small-caps/trunk@361384 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- small-caps.php
+++ small-caps.php
@@ -3,9 +3,9 @@
 Plugin Name: Small caps
 Plugin URI: http://wordpress.org/extend/plugins/small-caps/
 Description: Small caps for capitalized words. This plugin looks for sequences of 3 or more uppercase letters or numbers, such as CSS, HTML and W3C, and wraps an `&lt;abbr&gt;` tag around them. Credits to <a href="http://webtypography.net/">Webtypography</a> for the function, and for inspiration.
-Version: 1.0
-Author: Chetan Kunte
-Author URI: http://ckunte.com/
+Version: 1.0.1
+Author: Chyetanya Kunte
+Author URI: http://ckunte.net/
 */
 //
 // This function looks for sequences of 3 or more uppercase letters or numbers, 
@@ -22,19 +22,14 @@ if (function_exists('add_filter')) {
         add_filter('the_content', 'ckunte_smallcaps');
 }
 // 
-// Adding styling to template's head section. 
-// Intentionally excluded for users' freedom to style.
-// See: http://ckunte.com/archives/small-caps
-// 
 // Styling abbr to lowercase first, and then to small-caps.
 //
-// Uncomment following lines (take out those //s, if you prefer automatic styling as specified below.
-// function ckunte_smallcaps_wp_head () {
-//      echo "\n<style type=\"text/css\">abbr{text-transform:lowercase; font-variant:small-caps;}</style>\n";
-// }
-//
-// if (function_exists('add_action')) {
-//    add_action( 'wp_head', 'ckunte_smallcaps_wp_head' );
-// }
-//
+function ckunte_smallcaps_wp_head () {
+     echo "\n<style type=\"text/css\">abbr{text-transform:lowercase; font-variant:small-caps;}</style>\n";
+}
+
+if (function_exists('add_action')) {
+   add_action( 'wp_head', 'ckunte_smallcaps_wp_head' );
+}
+
 ?>
