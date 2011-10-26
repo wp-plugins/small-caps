95f270fee547b5e3a18afb7be4e25e4a29064abd
chetan <chetan@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-10-26 20:44:23 +0000

    Escaping caps inadvertently found in filenames, URLs, etc.
    git-svn-id: https://plugins.svn.wordpress.org/small-caps/trunk@456135 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- small-caps.php
+++ small-caps.php
@@ -1,18 +1,18 @@
 <?php
 /*
 Plugin Name: Small caps
-Plugin URI: http://wordpress.org/extend/plugins/small-caps/
+Plugin URI: http://github.com/ckunte/small-caps
 Description: Small caps for capitalized words. This plugin looks for sequences of 3 or more uppercase letters or numbers, such as CSS, HTML and W3C, and wraps an `&lt;abbr&gt;` tag around them. Credits to <a href="http://webtypography.net/">Webtypography</a> for the function, and for inspiration.
-Version: 1.0.1
-Author: Chyetanya Kunte
-Author URI: http://ckunte.net
+Version: 1.0.2
+Author: ckunte
+Author URI: http://github.com/ckunte
 */
 //
 // This function looks for sequences of 3 or more uppercase letters or numbers, 
 // such as CSS, HTML and W3C, and wraps an <abbr> tag around them.
 //
 function ckunte_smallcaps($text) {
-        $search = "/\b([A-Z][A-Z0-9]{2,})\b/";
+        $search = " /\b([A-Z][A-Z0-9]{2,})\b/ ";
         $replace = "<abbr>$1</abbr>";
         $text = preg_replace($search,$replace,$text);
         return $text;
@@ -22,14 +22,17 @@ if (function_exists('add_filter')) {
         add_filter('the_content', 'ckunte_smallcaps');
 }
 // 
+// Adding styling to template's head section. 
+// Intentionally excluded for users' freedom to style.
+// 
 // Styling abbr to lowercase first, and then to small-caps.
 //
+// Uncomment following lines (take out those //s, if you prefer automatic styling as specified below.
 function ckunte_smallcaps_wp_head () {
-     echo "\n<style type=\"text/css\">abbr{text-transform:lowercase; font-variant:small-caps;}</style>\n";
-}
-
-if (function_exists('add_action')) {
-   add_action( 'wp_head', 'ckunte_smallcaps_wp_head' );
-}
-
+      echo "\n<style>abbr{text-transform:lowercase; font-variant:small-caps;font-size:110%;}</style>\n";
+ }
+ if (function_exists('add_action')) {
+     add_action( 'wp_head', 'ckunte_smallcaps_wp_head' );
+ }
+//
 ?>
